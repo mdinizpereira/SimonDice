01e684a1c606cf2d46a04acc2af2901eb18d42b3
Miguel <migueldiniz25@gmail.com>
2016-12-17 16:16:40 +0100

    acabado layout e igualados los objetos del archivo java con el archivo xml

--- java/com/example/simoncolor/mainColores.java
+++ java/com/example/simoncolor/mainColores.java
@@ -1,14 +1,40 @@
 package com.example.simoncolor;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.support.v7.app.AppCompatActivity;
 
-public class mainColores extends AppCompatActivity {
+public class mainColores extends AppCompatActivity implements OnClickListener{
+    Button inicio, azul, rojo, verde, amarillo;
+    TextView explicacion;
+    MediaPlayer mpred, mpblue, mpyellow, mpgreen;
+    int vuelta = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_colores);
+        explicacion = (TextView) findViewById(R.id.explicacion);
+
+        inicio = (Button) findViewById(R.id.button);
+        azul = (Button) findViewById(R.id.blue);        //1
+        rojo = (Button) findViewById(R.id.red);         //2
+        verde = (Button) findViewById(R.id.green);      //3
+        amarillo = (Button) findViewById(R.id.yellow);  //4
+
+        azul.setOnClickListener(this);
+        rojo.setOnClickListener(this);
+        verde.setOnClickListener(this);
+        amarillo.setOnClickListener(this);
+        inicio.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+
     }
 
 
